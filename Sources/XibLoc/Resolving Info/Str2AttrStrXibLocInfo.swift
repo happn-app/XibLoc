699fcced0c47afb3b997f4df81aebc11de0713be
Frizlab <fload@me.com>
2018-05-30 17:29:10 +0200

    Enhance Str2AttrStrXibLocInfo

--- Sources/XibLoc/Resolving Info/Str2AttrStrXibLocInfo.swift
+++ Sources/XibLoc/Resolving Info/Str2AttrStrXibLocInfo.swift
@@ -26,15 +26,15 @@ public extension XibLocResolvingInfo where SourceType == String, ReturnType == N
 		if let f = baseFont  {defaultAttributesBuilding[.font] = f}
 		if let c = baseColor {defaultAttributesBuilding[.foregroundColor] = c}
 		
-		var attributesReplacementsBuilding = [String : StringAttributesChangesDescription]()
+		var attributesReplacementsBuilding = [OneWordTokens: StringAttributesChangesDescription]()
 		switch boldType {
-		case .default?:          attributesReplacementsBuilding["*"] = StringAttributesChangesDescription(changes: [.setBold])
-		case .custom(let font)?: attributesReplacementsBuilding["*"] = StringAttributesChangesDescription(changes: [.changeFont(newFont: font, preserveSizes: true, preserveBold: false, preserveItalic: true)])
+		case .default?:          attributesReplacementsBuilding[OneWordTokens(token: "*")] = StringAttributesChangesDescription(changes: [.setBold])
+		case .custom(let font)?: attributesReplacementsBuilding[OneWordTokens(token: "*")] = StringAttributesChangesDescription(changes: [.changeFont(newFont: font, preserveSizes: true, preserveBold: false, preserveItalic: true)])
 		default: (/*nop*/)
 		}
 		switch italicType {
-		case .default?:          attributesReplacementsBuilding["_"] = StringAttributesChangesDescription(changes: [.setItalic])
-		case .custom(let font)?: attributesReplacementsBuilding["_"] = StringAttributesChangesDescription(changes: [.changeFont(newFont: font, preserveSizes: true, preserveBold: true, preserveItalic: false)])
+		case .default?:          attributesReplacementsBuilding[OneWordTokens(token: "_")] = StringAttributesChangesDescription(changes: [.setItalic])
+		case .custom(let font)?: attributesReplacementsBuilding[OneWordTokens(token: "_")] = StringAttributesChangesDescription(changes: [.changeFont(newFont: font, preserveSizes: true, preserveBold: true, preserveItalic: false)])
 		default: (/*nop*/)
 		}
 		
@@ -44,7 +44,7 @@ public extension XibLocResolvingInfo where SourceType == String, ReturnType == N
 	/** Inits the Str2AttrStrXibLocInfo, copying the string resolving info from
 	`strResolvingInfo`. `simpleSourceTypeReplacements` is ignored from the string
 	resolving info. */
-	public init(strResolvingInfo: Str2StrXibLocInfo, attributesReplacements: [String: StringAttributesChangesDescription], returnTypeReplacements: [OneWordTokens: (_ originalValue: NSMutableAttributedString) -> NSMutableAttributedString]? = nil, defaultAttributes: [NSAttributedStringKey: Any]? = di.defaultStr2AttrStrAttributes) {
+	public init(strResolvingInfo: Str2StrXibLocInfo, attributesReplacements: [OneWordTokens: StringAttributesChangesDescription], returnTypeReplacements: [OneWordTokens: (_ originalValue: NSMutableAttributedString) -> NSMutableAttributedString]? = nil, defaultAttributes: [NSAttributedStringKey: Any]? = di.defaultStr2AttrStrAttributes) {
 		defaultPluralityDefinition = strResolvingInfo.defaultPluralityDefinition
 		escapeToken = strResolvingInfo.escapeToken
 		pluralGroups = strResolvingInfo.pluralGroups
@@ -53,7 +53,7 @@ public extension XibLocResolvingInfo where SourceType == String, ReturnType == N
 		
 		var attributesModificationsBuilding = Dictionary<OneWordTokens, (_ modified: inout NSMutableAttributedString, _ strRange: Range<String.Index>, _ refStr: String) -> Void>()
 		for (t, c) in attributesReplacements {
-			attributesModificationsBuilding[OneWordTokens(token: t)] = { attrStr, strRange, refStr in
+			attributesModificationsBuilding[t] = { attrStr, strRange, refStr in
 				assert(refStr == attrStr.string)
 				c.apply(to: attrStr, range: NSRange(strRange, in: refStr))
 			}
